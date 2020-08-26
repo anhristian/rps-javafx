3d896c60f8b41be6598d3eee83d8649b806d4c14
Anastasia Hristian <an.hristian@gmail.com>
2020-08-26 11:22:37 -0600

    Added the css style file.

--- src/main/java/edu/cnm/deepdive/controller/RpsController.java
+++ src/main/java/edu/cnm/deepdive/controller/RpsController.java
@@ -3,11 +3,13 @@ package edu.cnm.deepdive.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 
 public class RpsController {
 
 
+  @FXML private Label generation;
   @FXML private ToggleButton toggleRun;
   @FXML private Button reset; //this comes from rps.fxml file
 
